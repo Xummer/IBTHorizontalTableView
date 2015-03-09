9a60b990f5f284d846f56bf9e4643135c21a9312
xummer <willx@gobeta.com.cn>
2015-03-09 16:14:17 +0800

    Initial Commit

---
+++ IBTHorizontalTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IBTHorizontalTableView
+//
+//  Created by Xummer on 15/3/9.
+//  Copyright (c) 2015年 Xummer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IBTHorizontalTableView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IBTHorizontalTableView
+//
+//  Created by Xummer on 15/3/9.
+//  Copyright (c) 2015年 Xummer. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
